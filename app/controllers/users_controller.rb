909f5be07eda26ba051e190b1d1f7777bfa84678
jbudrow <jbudrowd3@yahoo.com>
2016-07-15 15:33:28 -0400

    create init file to load from lib, create api requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
+require 'pry'
 class UsersController < ApplicationController
   def new
-    User.new
+    binding.pry
   end
 
   def create
     @user = User.new(user_params)
+    # will have to add validation for gamertag availability
+    # TODO refactor using xbox.rb from lib/
+    @user.xuid = XboxApi::Client.new(ENV['X_AUTH']).gamer(@user.xbox_gamertag)
+
     if @user.save
       flash[:success] = "Register successful"
-      redirect_to @user
+      redirect_to :root
     else
       render :new
     end
